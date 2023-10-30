b3a4680b29f4e464e98abdd0f47113ad77dd8951
Alana <britoalanac@gmail.com>
2023-10-30 13:17:50 -0300

    enviando codigo

---
+++ alana.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class alana {
+
+    Run|Debug
+    public static void main (String[] args) throws Exception {
+        System.out.println(historia());
+        System.out.println(x:"Fim!");
+    }
+
+    public static int escolha(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(x:"");
+    }
+}
